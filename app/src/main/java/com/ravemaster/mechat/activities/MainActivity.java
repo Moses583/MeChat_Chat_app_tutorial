466ebc55e84590d86eb1ca200400bc5b9699a26a
Moses583 <moisendungu@gmail.com>
2024-08-28 16:04:25 +0300

    Completed sign out and update token feature of the application

--- app/src/main/java/com/ravemaster/mechat/activities/MainActivity.java
+++ app/src/main/java/com/ravemaster/mechat/activities/MainActivity.java
@@ -1,16 +1,40 @@
 package com.ravemaster.mechat.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.ravemaster.mechat.R;
+import com.ravemaster.mechat.utilities.Constants;
+import com.ravemaster.mechat.utilities.PreferenceManager;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtName;
+    ImageView imgLogOut,profileImage;
+    CardView one,two;
+    PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +46,79 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initViews();
+
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        loadUserDetails();
+        getToken();
+
+        imgLogOut.setOnClickListener(view -> {
+            signOut();
+        });
+
+    }
+
+    private void signOut(){
+        showToasts("Logging out...");
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference reference = database.collection(Constants.KEY_COLLECTION_USERS)
+                .document(preferenceManager.getString(Constants.KEY_USER_ID));
+
+        HashMap<String, Object> updates = new HashMap<>();
+        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
+        reference.update(updates)
+                .addOnSuccessListener(unused -> {
+                    preferenceManager.clear();
+                    startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+                    finish();
+                })
+                .addOnFailureListener(e -> {
+                    showToasts("Unable to log you out");
+                });
+
+    }
+
+    private void getToken(){
+        FirebaseMessaging.getInstance().getToken().addOnSuccessListener(this::updateToken);
+    }
+
+    private void updateToken(String token){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = database
+                .collection(Constants.KEY_COLLECTION_USERS)
+                .document(preferenceManager.getString(Constants.KEY_USER_ID));
+
+        documentReference.update(Constants.KEY_FCM_TOKEN, token)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        showToasts("Token updated successfully");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        showToasts("Unable to update token");
+                    }
+                });
+    }
+
+    private void showToasts(String message){
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    private void loadUserDetails(){
+        txtName.setText(preferenceManager.getString(Constants.KEY_NAME));
+        byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE),Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+        profileImage.setImageBitmap(bitmap);
+    }
+
+    private void initViews(){
+        imgLogOut = findViewById(R.id.imgLogOut);
+        txtName = findViewById(R.id.txtName);
+        one = findViewById(R.id.cardViewOne);
+        two = findViewById(R.id.cardViewTwo);
+        profileImage = findViewById(R.id.imgProfileImage);
     }
 }

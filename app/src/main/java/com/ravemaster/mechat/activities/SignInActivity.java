e0644c876d43f2e6e2f5e65060e7d627568edfbe
Moses583 <moisendungu@gmail.com>
2024-08-28 14:48:09 +0300

    Completed the sign in and sign up feature using firestore database and shared preferences

--- app/src/main/java/com/ravemaster/mechat/activities/SignInActivity.java
+++ app/src/main/java/com/ravemaster/mechat/activities/SignInActivity.java
@@ -2,8 +2,11 @@ package com.ravemaster.mechat.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,9 +19,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.ravemaster.mechat.R;
+import com.ravemaster.mechat.utilities.Constants;
+import com.ravemaster.mechat.utilities.PreferenceManager;
 
 import java.util.HashMap;
 
@@ -26,6 +33,10 @@ public class SignInActivity extends AppCompatActivity {
 
     TextView txtCreateAccount;
     Button button;
+    TextInputLayout enterEmail,enterPassword;
+    EditText one,two;
+    ProgressBar progressBar;
+    PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +49,14 @@ public class SignInActivity extends AppCompatActivity {
             return insets;
         });
         initViews();
+        initEditTexts();
+
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        if (preferenceManager.getBoolean(Constants.KEY_IS_SIGNED_IN)){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
         txtCreateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,10 +65,79 @@ public class SignInActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        button.setOnClickListener(view ->{
+            if (complete()){
+                signIn();
+            }
+        });
+
+    }
+
+    private void signIn() {
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .whereEqualTo(Constants.KEY_EMAIL,one.getText().toString())
+                .whereEqualTo(Constants.KEY_PASSWORD,two.getText().toString())
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful() && task.getResult() != null && !task.getResult().getDocuments().isEmpty()){
+                        DocumentSnapshot snapshot = task.getResult().getDocuments().get(0);
+
+                        preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN,true);
+                        preferenceManager.putString(Constants.KEY_USER_ID,snapshot.getId());
+                        preferenceManager.putString(Constants.KEY_NAME,snapshot.getString(Constants.KEY_NAME));
+                        preferenceManager.putString(Constants.KEY_IMAGE,snapshot.getString(Constants.KEY_IMAGE));
+
+                        Intent intent = new Intent(SignInActivity.this, MainActivity.class );
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    } else{
+                        loading(false);
+                        showToasts("Unable to log in, check your credentials");
+                    }
+                });
+    }
+
+    private Boolean complete(){
+        if (one.getText().toString().trim().isEmpty()){
+            showToasts("Please enter your email");
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(one.getText().toString()).matches()) {
+            showToasts("Enter a valid email address");
+            return  false;
+        } else if (two.getText().toString().trim().isEmpty()) {
+            showToasts("Please enter password");
+            return false;
+        }  else {
+            return true;
+        }
+    }
+
+    private void loading(boolean isLoading){
+        if (isLoading){
+            button.setVisibility(View.INVISIBLE);
+            progressBar.setVisibility(View.VISIBLE);
+        } else {
+            button.setVisibility(View.VISIBLE);
+            progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void initEditTexts() {
+        one = enterEmail.getEditText();
+        two = enterPassword.getEditText();
+    }
+
+    private void showToasts(String message){
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 
     private void initViews() {
         txtCreateAccount = findViewById(R.id.txtCreateAccount);
         button = findViewById(R.id.btnSignIn);
+        enterEmail = findViewById(R.id.enterEmail);
+        enterPassword = findViewById(R.id.enterPassword);
+        progressBar = findViewById(R.id.myProgressBar2);
     }
 }
